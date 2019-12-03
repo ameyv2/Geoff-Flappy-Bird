59aa08dafd91ada243f2daa65728d52a02f469eb
Amey Venkatanarayan <amey.venkat@gmail.com>
2019-12-03 17:47:16 -0600

    Added music

--- app/src/main/java/com/example/geoffflappybird/MainActivity.java
+++ app/src/main/java/com/example/geoffflappybird/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.geoffflappybird;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
     private GeoffFlappyView gameView;
@@ -11,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         gameView = new GeoffFlappyView(this);
         setContentView(gameView);
+
+        MediaPlayer player;
+        player = MediaPlayer.create(this, R.raw.flightsong);
+        player.setLooping(true);
+        player.start();
     }
 }
